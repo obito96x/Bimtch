693569955803d1b7f52712251c0c18a5e6905039
obito96x <uffobitoxe@gmail.com>
2025-01-09 16:06:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,10 +6,25 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, ABOUT_TXT, HELP_TXT, FORCE_PIC
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, ABOUT_TXT, HELP_TXT, AUTO_DEL, DEL_TIMER, DEL_MSG, FORCE_PIC
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
+from plugins.autodelete import convert_time
 
+
+# Autodelete notification function
+async def auto_del_notification(client, msg, delay_time):
+    if AUTO_DEL.lower() == "true":
+        await msg.reply_text(DEL_MSG.format(time=convert_time(DEL_TIMER)))
+        await asyncio.sleep(delay_time)
+        await msg.delete()
+
+# Delete message after delay function
+async def delete_message(msg, delay_time):
+    if AUTO_DEL.lower() == "true":
+        await asyncio.sleep(delay_time)
+        await msg.delete()
+        
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
