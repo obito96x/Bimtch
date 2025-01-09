2a6fb45ce11e92e3b84480728c6fa27c493a6dd6
obito96x <uffobitoxe@gmail.com>
2025-01-09 18:26:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,7 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, ABOUT_TXT, HELP_TXT, AUTO_DEL, DEL_TIMER, DEL_MSG, FORCE_PIC
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
+from database.database import present_admin
 from plugins.chumtiya import convert_time
 
 
